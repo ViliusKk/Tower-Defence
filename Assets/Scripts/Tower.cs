16b154de919a9e083bb24fad9f11231fecfa7cc7
ViliusKk <vilius.kuokstas@gmail.com>
2025-03-09 15:32:28 +0200

    Enemy tower

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            print("good");
+        }
     }
 }
--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -1,17 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerBuilder : MonoBehaviour
 {
+    public LayerMask groundLayer;
     public Transform cursor;
+    public GameObject towerPrefab;
     
     void Update()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        if (Physics.Raycast(ray, out RaycastHit hit, groundLayer))
         {
             cursor.position = hit.point;
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(towerPrefab, cursor.position, cursor.rotation);
+            }
         }
     }
 }
