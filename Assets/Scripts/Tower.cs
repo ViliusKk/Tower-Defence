9fa63e90deb5231dd5709c3aa8ca3563a4ec3fe6
ViliusKk <vilius.kuokstas@gmail.com>
2025-03-10 19:11:32 +0200

    Tower shooting

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,16 +1,39 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float cooldown = 1f;
+    
+    public List<GameObject> enemies;
     void Start()
     {
-        
+        InvokeRepeating("Fire", 0, cooldown);
     }
-    void Update()
+
+    void Fire()
     {
-        
+        CleanTargets();
+        if (enemies.Count == 0) return;
+        var target = enemies[Random.Range(0, enemies.Count)];
+        var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Bullet>().target = target.transform;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Enemy")) enemies.Add(other.gameObject);
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.CompareTag("Enemy")) enemies.Remove(other.gameObject);
+    }
+
+    void CleanTargets()
+    {
+        enemies.RemoveAll(enemy => enemy == null);
     }
 }
