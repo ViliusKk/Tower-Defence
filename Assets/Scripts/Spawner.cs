6531e5a9013852326d67235a1bf67ba2db2e3158
ViliusKk <vilius.kuokstas@gmail.com>
2025-03-16 14:57:14 +0200

    Enemy walk path

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject prefab;
+    
+    [Header("Wave settings")]
     public int count = 10;
     public float cooldown = 0.5f;
+
+    [Header("Spawn settings")]
+    public int waveCount = 10;
+    public float waveCooldown = 5f;
+    
     void Start()
     {
         StartCoroutine(Spawn());
@@ -14,10 +21,17 @@ public class Spawner : MonoBehaviour
 
     IEnumerator Spawn()
     {
-        for (int i = 0; i < count; i++)
+        // all waves
+        for (int i = 0; i < waveCount; i++)
         {
-            Instantiate(prefab, transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(cooldown);
+            // one wave
+            for (int j = 0; j < count; j++)
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(cooldown);
+            }
+            
+            yield return new WaitForSeconds(waveCooldown);
         }
     }
 }
