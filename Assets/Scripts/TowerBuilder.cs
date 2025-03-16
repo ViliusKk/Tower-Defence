26b86b1936d6c04d938fbb51ef625fc98b2c1487
ViliusKk <vilius.kuokstas@gmail.com>
2025-03-16 15:07:52 +0200

    Tower colors

--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -5,14 +5,24 @@ public class TowerBuilder : MonoBehaviour
     public LayerMask groundLayer;
     public Transform cursor;
     public GameObject towerPrefab;
+    public Color canBuildColor = Color.green;
+    public Color noBuildColor = Color.red;
+
+    private bool canBuild;
     
     void Update()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, groundLayer))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             cursor.position = hit.point;
-            if (Input.GetMouseButtonDown(0))
+            
+            canBuild = hit.transform.CompareTag("Ground");
+            
+            if(canBuild) cursor.GetComponent<Renderer>().material.color = canBuildColor;
+            else cursor.GetComponent<Renderer>().material.color = noBuildColor;
+            
+            if (Input.GetMouseButtonDown(0) && canBuild)
             {
                 Instantiate(towerPrefab, cursor.position, cursor.rotation);
             }
