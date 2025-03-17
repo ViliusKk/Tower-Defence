d2f6fcdf57b6c917192f0ed909b436eda962770b
ViliusKk <vilius.kuokstas@gmail.com>
2025-03-17 18:56:47 +0200

    Link tower upgrades

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    public List<TowerUpgrade> upgrades;
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float cooldown = 1f;
     
     public List<GameObject> enemies;
+
+    private int currentUpgrade = 0;
     void Start()
     {
         InvokeRepeating("Fire", 0, cooldown);
@@ -35,5 +38,21 @@ public class Tower : MonoBehaviour
     void CleanTargets()
     {
         enemies.RemoveAll(enemy => enemy == null);
+    }
+
+    public void UpgradeTower()
+    {
+        if (Player.instance.Money >= upgrades[currentUpgrade].cost)
+        {
+            cooldown = upgrades[currentUpgrade].cooldown;
+            if(upgrades[currentUpgrade].bulletPrefab != null) bulletPrefab = upgrades[currentUpgrade].bulletPrefab;
+            if (upgrades[currentUpgrade].towerPrefab != null)
+            {
+                Instantiate(upgrades[currentUpgrade].towerPrefab, transform.position, Quaternion.identity);
+            }
+            
+            Player.instance.Money -= upgrades[currentUpgrade].cost;
+            currentUpgrade++;
+        }
     }
 }
