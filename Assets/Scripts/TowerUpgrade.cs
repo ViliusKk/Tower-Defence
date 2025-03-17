b6fe9e8e051b19d513c8e2395d3a07836b30384c
ViliusKk <vilius.kuokstas@gmail.com>
2025-03-17 18:49:58 +0200

    Tower upgrades

--- Assets/Scripts/TowerUpgrade.cs
+++ Assets/Scripts/TowerUpgrade.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Tower Upgrade",menuName = "Tower Upgrade")]
+
 public class TowerUpgrade : ScriptableObject
 {
-    
+    public int cost;
+    public float cooldown;
+    public GameObject towerPrefab;
+    public GameObject bulletPrefab;
+    public Sprite icon;
 }
